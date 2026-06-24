8068aca9fa08453e80c6841ca4bcdb00f5f3f4a7
Odvin <odvinenriquerivera12345@gmail.com>
2026-06-24 13:12:15 -0600

    Fix: CORS abierto temporalmente

--- server.js
+++ server.js
@@ -54,13 +54,7 @@ function generateRandomPassword(length = 12) {
 }
 
 app.use(cors({
-    origin: [
-        'https://invensaas-sistema.vercel.app/', 
-        'https://sistema-inventario-gilt.vercel.app',
-        'http://localhost:5500',
-        'http://127.0.0.1:5500'
-    ],
-    credentials: true,
+    origin: '*', // ⚠️ Temporalmente permitir todas las IPs para pruebas
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
